3fedc0c3abb1ab26475b59e12efe24281b46af1b
Luke <lukevgomes@gmail.com>
2015-10-08 16:56:21 +0530

    d

---
+++ src/com/luke/dto6/User.java
@@ -0,0 +1,51 @@
+package com.luke.dto6;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table (name="USER_Details")
+public class User {
+	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private int userId;
+	private String userName;
+	@ElementCollection
+	private Set<Address> lOfAddresses = new HashSet();
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Set<Address> getlOfAddresses() {
+		return lOfAddresses;
+	}
+	public void setlOfAddresses(Set<Address> lOfAddresses) {
+		this.lOfAddresses = lOfAddresses;
+	}
+		
+}
